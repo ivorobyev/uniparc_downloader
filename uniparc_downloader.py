fd8da3a46a97b7f3b00edf42fd70493bff2f309d
ivorobyev <vorobyevilia@gmail.com>
2019-11-13 14:47:36 +0300

    add data type cheking

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -2,6 +2,14 @@ import requests
 import argparse
 import time
 
+def tab_processing(request_result):
+    raw_table = str(request_result.text).split('\n')
+    f_table = []
+    for a in raw_table:
+        f_table.append(a.split('\t'))
+
+    return f_table
+
 start_time = time.time()
 
 parser = argparse.ArgumentParser()
@@ -9,20 +17,23 @@ parser.add_argument('-t', '--taxon', help = 'organism identifier')
 parser.add_argument('-f', '--format', help = 'output format')
 args = parser.parse_args()
 
+formats = ['tab', 'fasta']
+if args.format not in formats:
+    raise NameError('unknown data format') 
+
 base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}'
 
 req = base_url.format(args.taxon, args.format)
 
 print('collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
 
-s = requests.get(req)
+request_data = requests.get(req)
 
 #parse given data
-m = str(s.text)
-m = m.split('\n')
-f_table = []
-for a in m:
-    f_table.append(a.split('\t'))
+if args.format == 'tab':
+    f_table = tab_processing(request_data)
+elif args.format == 'fasta':
+    raise NameError('no fasta processing yet')
 
 #write in file
 file_name = 'uniparc_{0}_{1}_{2}'.format(args.taxon, args.format, time.time())
