0ae93dcaed137203335b180eb5e29a0d07cb7338
ivorobyev <vorobyevilia@gmail.com>
2019-11-13 10:25:58 +0300

    add request

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -1,6 +1,6 @@
 import requests
-import pandas as pd
 
 taxon = 33926
 format_ = 'fasta'
 r = requests.get('https://www.uniprot.org/uniparc/?query=organism:'+str(taxon)+'&format='+format_)
+print(r.content)
