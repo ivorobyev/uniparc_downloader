d23c47720cba9c7ae9af3ec2136822004ac0a87c
ivorobyev <vorobyevilia@gmail.com>
2019-11-13 17:51:54 +0300

    add possibility to download large files

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -2,14 +2,6 @@ import requests
 import argparse
 import time
 
-def tab_processing(request_result):
-    raw_table = str(request_result.text).split('\n')
-    f_table = []
-    for a in raw_table:
-        f_table.append(a.split('\t'))
-
-    return f_table
-
 start_time = time.time()
 
 parser = argparse.ArgumentParser()
@@ -27,20 +19,19 @@ req = base_url.format(args.taxon, args.format)
 
 print('collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
 
-request_data = requests.get(req)
+#define output filename
+filename = 'uniparc_{0}_{1}_{2}.csv'.format(args.taxon, args.format, time.time())
 
-#parse given data
-if args.format == 'tab':
-    f_table = tab_processing(request_data)
-elif args.format == 'fasta':
-    raise NameError('no fasta processing yet')
+def download_file(url, filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                if chunk:
+                    f.write(chunk)
+    print('File downloaded')
 
-#write in file
-file_name = 'uniparc_{0}_{1}_{2}'.format(args.taxon, args.format, time.time())
-with open(file_name, 'w') as f:
-    for a in f_table:
-        f.write(str(a) + '\n')
-    f.close()
+download_file(req, filename)
 
 print('elapsed_time: ' + str(time.time() - start_time))
-print('data saved in file: ' + file_name)
+print('data saved in file: ' + filename)
