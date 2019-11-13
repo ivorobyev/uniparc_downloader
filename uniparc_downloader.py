9ab9d01b470de49bc737ae4ec72f3d341d6e6b0f
ivorobyev <vorobyevilia@gmail.com>
2019-11-13 13:57:01 +0300

    some fixes

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -2,12 +2,11 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--taxon', help = 'organism identificator')
+parser.add_argument('-t', '--taxon', help = 'organism identifier')
 parser.add_argument('-f', '--format', help = 'output format')
 args = parser.parse_args()
 
 base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}'
-
 
 req = base_url.format(args.taxon, args.format)
 
