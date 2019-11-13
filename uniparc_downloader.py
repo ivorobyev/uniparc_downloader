107f74b5728d33075c5edf75eabe6feb2a921e37
ivorobyev <vorobyevilia@gmail.com>
2019-11-13 14:20:29 +0300

    write in file

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -1,5 +1,8 @@
 import requests
 import argparse
+import time
+
+start_time = time.time()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--taxon', help = 'organism identifier')
@@ -10,11 +13,23 @@ base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}'
 
 req = base_url.format(args.taxon, args.format)
 
+print('collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
+
 s = requests.get(req)
+
+#parse given data
 m = str(s.text)
 m = m.split('\n')
 f_table = []
 for a in m:
     f_table.append(a.split('\t'))
 
-print(f_table)
+#write in file
+file_name = 'uniparc_{0}_{1}_{2}'.format(args.taxon, args.format, time.time())
+with open(file_name, 'w') as f:
+    for a in f_table:
+        f.write(str(a) + '\n')
+    f.close()
+
+print('elapsed_time: ' + str(time.time() - start_time))
+print('data saved in file: ' + file_name)
