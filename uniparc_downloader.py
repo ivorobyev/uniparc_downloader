0b9fa5cc370907664a18338021c70278227bd909
ivorobyev <vorobyevilia@gmail.com>
2019-11-14 14:19:32 +0300

    add exceptions and colored output

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -11,16 +11,21 @@ args = parser.parse_args()
 
 formats = ['tab', 'fasta']
 if args.format not in formats:
-    raise NameError('unknown data format') 
+    raise NameError('\033[91m' + 'ERROR: unknown data format' + '\033[91m') 
 
 #make url for request
-base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}&columns:Sequence'
+base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}&columns:sequence'
 req = base_url.format(args.taxon, args.format)
 
 #print version
-r = requests.head(req)
-print('X-UniProt-Release: ' + r.headers['X-UniProt-Release'])
-print('Last-Modified: ' + r.headers['Last-Modified'])
+try:
+    r = requests.head(req)
+    print('UniProt Release: ' + r.headers['X-UniProt-Release'])
+    print('Last Modified: ' + r.headers['Last-Modified'])
+    print('Total rows: ' + r.headers['X-Total-Results'])
+except:
+    print('\033[91m' + 'CONNECTION ERROR: server unavailable' + '\033[91m') 
+    exit()
 
 print('Collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
 
@@ -28,15 +33,19 @@ print('Collecting data for taxon-{0} in {1} format'.format(args.taxon, args.form
 filename = 'uniparc_{0}_{1}_{2}.csv'.format(args.taxon, args.format, time.time())
 
 def download_file(url, filename):
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                if chunk:
-                    f.write(chunk)
-    print('File downloaded')
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    if chunk:
+                        f.write(chunk)
+        print('Download complete')
+    except:
+        print('\033[91m' + 'CONNECTION ERROR: server unavailable' + '\033[91m')
+        exit()
 
 download_file(req, filename)
-
-print('elapsed_time: ' + str(time.time() - start_time))
-print('data saved in file: ' + filename)
+    
+print('\033[92m' + 'elapsed_time: ' + str(time.time() - start_time) + '\033[92m')
+print('\033[92m' + 'data saved in file: ' + filename + '\033[92m')
