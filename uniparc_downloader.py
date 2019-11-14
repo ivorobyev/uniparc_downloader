897f5aed71c0b35968cfe3b75ffdd7259ab60617
ivorobyev <vorobyevilia@gmail.com>
2019-11-14 14:44:54 +0300

    code refactoring

--- uniparc_downloader.py
+++ uniparc_downloader.py
@@ -13,36 +13,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-start_time = time.time()
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--taxon', help = 'organism identifier')
-parser.add_argument('-f', '--format', help = 'output format')
-args = parser.parse_args()
-
-formats = ['tab', 'fasta']
-if args.format not in formats:
-    raise NameError(bcolors.FAIL + 'ERROR: unknown data format' + bcolors.ENDC) 
-
-#make url for request
-base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}&columns:sequence'
-req = base_url.format(args.taxon, args.format)
-
-#print version
-try:
-    r = requests.head(req)
-    print('UniProt Release: ' + r.headers['X-UniProt-Release'])
-    print('Last Modified: ' + r.headers['Last-Modified'])
-    print('Total rows: ' + r.headers['X-Total-Results'])
-except:
-    print(bcolors.FAIL + 'CONNECTION ERROR: server unavailable' + bcolors.ENDC) 
-    exit()
-
-print('Collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
-
-#define output filename
-filename = 'uniparc_{0}_{1}_{2}.csv'.format(args.taxon, args.format, time.time())
-
+#function to download file by chunks
 def download_file(url, filename):
     try:
         with requests.get(url, stream=True) as r:
@@ -56,7 +27,41 @@ def download_file(url, filename):
         print(bcolors.FAIL + 'CONNECTION ERROR: server unavailable' + bcolors.ENDC)
         exit()
 
-download_file(req, filename)
-    
-print(bcolors.OKGREEN  + 'elapsed_time: ' + str(time.time() - start_time) + bcolors.ENDC)
-print(bcolors.OKGREEN + 'data saved in file: ' + filename + bcolors.ENDC)
+if __name__ == '__main__' :
+    print(bcolors.OKBLUE + 'Uniparc downloader runing' + bcolors.ENDC)
+
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--taxon', help = 'organism identifier')
+    parser.add_argument('-f', '--format', help = 'output format')
+    args = parser.parse_args()
+
+    formats = ['tab', 'fasta']
+    if args.format not in formats:
+        raise NameError(bcolors.FAIL + 'ERROR: unknown data format' + bcolors.ENDC) 
+
+    #make url for request
+    base_url = 'https://www.uniprot.org/uniparc/?query=organism:{0}&format={1}&columns:sequence'
+    req = base_url.format(args.taxon, args.format)
+
+    #print version
+    try:
+        r = requests.head(req)
+        print('UniProt Release: ' + r.headers['X-UniProt-Release'])
+        print('Last Modified: ' + r.headers['Last-Modified'])
+        print('Total rows: ' + r.headers['X-Total-Results'])
+    except:
+        print(bcolors.FAIL + 'CONNECTION ERROR: server unavailable' + bcolors.ENDC) 
+        exit()
+
+    print('Collecting data for taxon-{0} in {1} format'.format(args.taxon, args.format))
+
+    #define output filename
+    filename = 'uniparc_{0}_{1}_{2}.csv'.format(args.taxon, args.format, time.time())
+
+    #downloading file
+    download_file(req, filename)
+        
+    print(bcolors.OKGREEN  + 'elapsed_time: ' + str(time.time() - start_time) + bcolors.ENDC)
+    print(bcolors.OKGREEN + 'data saved in file: ' + filename + bcolors.ENDC)
